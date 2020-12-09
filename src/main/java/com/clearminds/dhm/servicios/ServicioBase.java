0eaa3019319c5e44fbe643eb4cb77c0b1a82d1da
Dario Herrera <dario97herrera@example.com>
2020-12-09 10:50:38 -0500

    Añadir archivos SERVICIOS

---
+++ src/main/java/com/clearminds/dhm/servicios/ServicioBase.java
@@ -0,0 +1,38 @@
+package com.clearminds.dhm.servicios;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import com.clearminds.dhm.bdd.ConexionBDD;
+import com.clearminds.dhm.excepciones.BDDException;
+
+public class ServicioBase {
+	private Connection conexion;
+
+	
+	public Connection getConexion() {
+		return conexion;
+	}
+
+	public void setConexion(Connection conexion) {
+		this.conexion = conexion;
+	}
+
+	public void abrirConexion() throws BDDException{
+		this.conexion = ConexionBDD.obtenerConexion();
+	}
+	
+	public void cerrarConexion(){
+		try {
+			if(this.conexion != null){
+				this.conexion.close();
+				System.out.println("Conexion cerrada");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("Error al cerrar la conexion");
+		}
+	}
+	
+	
+}
---
+++ src/main/java/com/clearminds/dhm/servicios/ServicioEstudiante.java
@@ -0,0 +1,18 @@
+package com.clearminds.dhm.servicios;
+
+import com.clearminds.dhm.excepciones.BDDException;
+
+public class ServicioEstudiante extends ServicioBase{
+	
+	@Override
+	public void abrirConexion() throws BDDException {
+		// TODO Auto-generated method stub
+		super.abrirConexion();
+	}
+	
+	@Override
+	public void cerrarConexion() {
+		// TODO Auto-generated method stub
+		super.cerrarConexion();
+	}
+}
