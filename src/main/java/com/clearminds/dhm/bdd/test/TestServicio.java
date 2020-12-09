1ca27df5c6783a841b1dd03367bbfb23d76c4e58
Dario Herrera <dario97herrera@example.com>
2020-12-09 11:45:29 -0500

    Se inserta estudiantes a la BDD

--- src/main/java/com/clearminds/dhm/bdd/test/TestServicio.java
+++ src/main/java/com/clearminds/dhm/bdd/test/TestServicio.java
@@ -9,7 +9,7 @@ public class TestServicio {
 	public static void main(String[] args) {
 		ServicioEstudiante srvEstudiante = new ServicioEstudiante();
 		try {
-			srvEstudiante.insertarEstudiante(new Estudiante("Dario", "Herrera"));
+			srvEstudiante.insertarEstudiante(new Estudiante("Luis", "Segovia"));
 		} catch (BDDException e) {
 			e.printStackTrace();
 			e.getMessage();
--- src/main/java/com/clearminds/dhm/servicios/ServicioEstudiante.java
+++ src/main/java/com/clearminds/dhm/servicios/ServicioEstudiante.java
@@ -1,26 +1,43 @@
 package com.clearminds.dhm.servicios;
 
+import java.sql.SQLException;
+import java.sql.Statement;
+
 import com.clearminds.dhm.bdd.ConexionBDD;
 import com.clearminds.dhm.dtos.Estudiante;
 import com.clearminds.dhm.excepciones.BDDException;
 
-public class ServicioEstudiante extends ServicioBase{
-	
+public class ServicioEstudiante extends ServicioBase {
+
 	@Override
 	public void abrirConexion() throws BDDException {
 		// TODO Auto-generated method stub
 		super.abrirConexion();
 	}
-	
+
 	@Override
 	public void cerrarConexion() {
 		// TODO Auto-generated method stub
 		super.cerrarConexion();
 	}
-	
-	public void insertarEstudiante(Estudiante est) throws BDDException{
+
+	public void insertarEstudiante(Estudiante est) throws BDDException {
 		abrirConexion();
-		System.out.println("Insertando Estudiante: " +est);
-		cerrarConexion();
+		System.out.println("Insertando Estudiante: " + est);
+		Statement stmt = null;
+		try {
+			stmt = getConexion().createStatement();
+
+			String sql = "insert into estudiantes(nombre,apellido) values('" + est.getNombre() + "','"
+					+ est.getApellido() + "')";
+			System.out.println("Script: " + sql);
+
+			stmt.executeUpdate(sql);
+			cerrarConexion();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new BDDException("Error al insertar estudiante");
+		}
+
 	}
 }
